0617ce0cbd92e93541b93689a6091e35baef9e32
brandonTECVER <brandontecver@gmail.com>
2025-04-28 19:45:58 -0600

    subir sql

--- 3ro/guardar_encuesta.php
+++ 3ro/guardar_encuesta.php
@@ -22,6 +22,11 @@ $proyectos = $_POST['proyectos'];
 $investigacion = $_POST['investigacion'];
 $infraestructura = $_POST['infraestructura'];
 $residencia = $_POST['residencia'];
+$actividad = $_POsT['actividad'];
+$actividad = $_POST['actividad'];
+$actividad = $_POST['actividad'];
+$
+
 
 // Verifica si ya existe un registro con ese no_control
 $verifica = "SELECT * FROM encuesta WHERE no_control = '$no_control'";
@@ -29,7 +34,7 @@ $resultado = $conexion->query($verifica);
 
 if ($resultado->num_rows > 0) {
     // Ya existe, no se permite duplicado
-    echo "⚠️ Ya has enviado esta sección de la encuesta. No es necesario volver a llenarla.";
+    echo " Ya has enviado esta sección de la encuesta. No es necesario volver a llenarla.";
 } else {
     // Insertar datos
     $sql = "INSERT INTO encuesta (
---
+++ alumnos_db.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-04-2025 a las 03:42:02
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `alumnos_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuesta`
+--
+
+CREATE TABLE `encuesta` (
+  `id` int(11) NOT NULL,
+  `no_control` varchar(20) DEFAULT NULL,
+  `docentes` varchar(20) DEFAULT NULL,
+  `plan_estudios` varchar(20) DEFAULT NULL,
+  `proyectos` varchar(20) DEFAULT NULL,
+  `investigacion` varchar(20) DEFAULT NULL,
+  `infraestructura` varchar(20) DEFAULT NULL,
+  `residencia` varchar(20) DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `no_control` varchar(20) NOT NULL,
+  `nip` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `no_control`, `nip`) VALUES
+(1, '1234', '5678'),
+(2, '4321', '8765');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `encuesta`
+--
+ALTER TABLE `encuesta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `encuesta`
+--
+ALTER TABLE `encuesta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
